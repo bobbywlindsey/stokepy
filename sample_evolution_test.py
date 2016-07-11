40c0b00ca48ad097a93e0d192e0f564d0de514d5
Bobby <me@bobbywlindsey.com>
2016-07-10 23:48:08 -0500

    added method to generate Markov chain from parameters; added boundary conditions

--- sample_evolution_test.py
+++ sample_evolution_test.py
@@ -1,36 +1,42 @@
 import stokepy as sp
 import numpy as np
 
-# define transition matrix
-P = np.array([[0, .5, 0, .5, 0, 0, 0, 0, 0], \
-              [1/3, 0, 1/3, 0, 1/3, 0, 0, 0, 0], \
-              [0, .5, 0, 0, 0, .5, 0, 0, 0], \
-              [1/3, 0, 0, 0, 1/3, 0, 1/3, 0, 0], \
-              [0, 1/4, 0, 1/4, 0, 1/4, 0, 1/4, 0], \
-              [0, 0, 1/3, 0, 1/3, 0, 0, 0, 1/3], \
-              [0, 0, 0, 0, 0, 0, 1, 0, 0], \
-              [0, 0, 0, 0, 1/3, 0, 1/3, 0, 1/3], \
-              [0, 0, 0, 0, 0, 0, 0, 0, 1]])
-              
-# define initial distribution
-phi = np.array([0, 0, 0, 0, 0, 1, 0, 0, 0])
+# create nxn transition matrix
+# P = np.array([[0, .5, 0, .5, 0, 0, 0, 0, 0], \
+#               [1/3, 0, 1/3, 0, 1/3, 0, 0, 0, 0], \
+#               [0, .5, 0, 0, 0, .5, 0, 0, 0], \
+#               [1/3, 0, 0, 0, 1/3, 0, 1/3, 0, 0], \
+#               [0, 1/4, 0, 1/4, 0, 1/4, 0, 1/4, 0], \
+#               [0, 0, 1/3, 0, 1/3, 0, 0, 0, 1/3], \
+#               [0, 0, 0, 0, 0, 0, 1, 0, 0], \
+#               [0, 0, 0, 0, 1/3, 0, 1/3, 0, 1/3], \
+#               [0, 0, 0, 0, 0, 0, 0, 0, 1]])
 
-# create Markov chain
-markov_chain = sp.MarkovChain(P, phi)
+# instantiate class
+mc = sp.MarkovChain()
+# generate Markov chain with no boundary conditions
+mc.gen_from_params(p = 0.6, num_of_states = 5, dim = 2)
+print(mc.P)
+mc.apply_boundary_condition(condition='emi-reflecting')
+print(mc.P)
+# apply boundary condition (only works for 1d atm)
 
-# specify SampleEvolution parameters
-samples                     = 2000
-steps                       = 1000
-states_in_recurrent_classes = [[6], [8]]
 
-# instantiate solution; memory and tolerance are optional parameters
-sample_evolution = sp.SampleEvolution(markov_chain, samples, steps, \
-                                      states_in_recurrent_classes)
-# run simulation
-sample_evolution.run()
-
-# sample_evolution now has data from the results of its simulation
-average_distribution = sample_evolution.pi
-
-# you can even plot absorption times
-sample_evolution.plot_absorption()
+# # create initial distribution vector
+# phi = np.array([0, 0, 0, 0, 0, 1, 0, 0, 0])
+#
+# # create Markov chain object
+# markov_chain = sp.MarkovChain(P, phi)
+#
+# # choose solution method like Sample Evolution
+# sample_evolution = sp.SampleEvolution(markov_chain, samples, steps, states_in_recurrent_classes)
+#
+# # run the solution
+# sample_evolution.run()
+#
+# # get data from the run
+# average_distribution   = sample_evolution.pi
+# epdf                   = sample_evolution.epdf
+# absorption_proportions = sample_evolution.absorption_proportions
+# apbrc                  = sample_evolution.recurrent_class_absorbed_proportions
+# mean_absorption_time   = sample_evolution.mean_absorption_time
--- stokepy/markov_chain.py
+++ stokepy/markov_chain.py
@@ -1,3 +1,5 @@
+from .headers import *
+
 class MarkovChain:
     """
     Creates Markov Chain object
@@ -7,11 +9,77 @@ class MarkovChain:
     - add function to auto-generate P
     """
 
-    def __init__(self, P, phi):
+    def __init__(self):
         """
         P = transition matrix
         phi = initial distribution
         """
 
-        self.P   = P
-        self.phi = phi
+        self.P             = None
+        self.p             = None
+        self.q             = None
+        self.num_of_states = None
+        self.dim           = None
+
+    def gen_from_params(self, p, num_of_states, dim):
+        """
+        p = probability of moving forward to another state
+        dim = dimension of the Markov chain
+
+        returns transition matrix, P
+        """
+        q                  = 1 - p
+        self.p             = p
+        self.q             = q
+        self.num_of_states = num_of_states
+        self.dim           = dim
+        if dim == 1:
+            P = np.zeros([num_of_states, num_of_states])
+            for row in range(num_of_states):
+                if row+1 != num_of_states-1:
+                    P[row+1, row]   = q
+                    P[row+1, row+2] = p
+                else:
+                    self.P = P
+                    break
+        else:
+            raise ValueError('Not yet support for {} dimensions'.format(self.dim))
+
+    def apply_boundary_condition(self, condition):
+        if dim == 1:
+            if condition == 'absorbing':
+                # create beginning boundary
+                beg_boundary     = np.zeros(self.num_of_states)
+                beg_boundary[0]  = 1
+                # create end boundary
+                end_boundary     = np.zeros(self.num_of_states)
+                end_boundary[-1] = 1
+                # apply boundaries to transition matrix
+                self.P[0]  = beg_boundary
+                self.P[-1] = end_boundary
+            elif condition == 'reflecting':
+                # create beginning boundary
+                beg_boundary     = np.zeros(self.num_of_states)
+                beg_boundary[1]  = 1
+                # create end boundary
+                end_boundary     = np.zeros(self.num_of_states)
+                end_boundary[-2] = 1
+                # apply boundaries to transition matrix
+                self.P[0]  = beg_boundary
+                self.P[-1] = end_boundary
+            elif condition == 'semi-reflecting':
+                # create beginning boundary
+                beg_boundary     = np.zeros(self.num_of_states)
+                beg_boundary[0]  = self.q
+                beg_boundary[1]  = self.p
+                # create end boundary
+                end_boundary     = np.zeros(self.num_of_states)
+                end_boundary[-1] = self.p
+                end_boundary[-2] = self.q
+                # apply boundaries to transition matrix
+                self.P[0]  = beg_boundary
+                self.P[-1] = end_boundary
+            else:
+                raise ValueError('{} is not a supported condition'.format(condition))
+        else:
+            raise ValueError('Not yet support for {} dimensions'.format(self.dim))
