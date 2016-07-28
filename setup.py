bea2799f5281990ca1cfd11ca7390febc0232960
bobbywlindsey <me@bobbywlindsey.com>
2016-07-28 14:03:18 -0500

    updated setup file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='stokepy',
       version='0.1',
       description='Stochastic models library for Python',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/bobbywlindsey/stokepy',
       author='Bobby Lindsey',
       author_email='me@bobbywlindsey.com',
       license='MIT',
