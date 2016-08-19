e7f4ad0c27d8fc22129558db6ae0dbbab0627ace
bobbywlindsey <me@bobbywlindsey.com>
2016-08-18 22:56:16 -0500

    converted tabs to spaces due to Python guidelines

--- stokepy/metropolis.py
+++ stokepy/metropolis.py
@@ -1,118 +1,118 @@
 from .helpers import *
 
 class Metropolis:
-	"""
-	parameters:
-		num_steps (int) = number of steps to take
-		num_samples (int) = number of samples
-		ciphered_text (str) = ciphered text you want to decipher
-	"""
+    """
+    parameters:
+        num_steps (int) = number of steps to take
+        num_samples (int) = number of samples
+        ciphered_text (str) = ciphered text you want to decipher
+    """
 
-	def __init__(self, num_steps, num_samples, ciphered_text):
-		self.num_steps           = num_steps
-		self.num_samples         = num_samples
-		self.ciphered_text       = ciphered_text
-		self.ciphered_text_len   = len(self.ciphered_text)
+    def __init__(self, num_steps, num_samples, ciphered_text):
+        self.num_steps           = num_steps
+        self.num_samples         = num_samples
+        self.ciphered_text       = ciphered_text
+        self.ciphered_text_len   = len(self.ciphered_text)
 
-		self.unique_symbols      = None
-		self.ciphered_numeric    = None
-		self.num_unique_symbols  = None
-		self.target_plausibility = None
+        self.unique_symbols      = None
+        self.ciphered_numeric    = None
+        self.num_unique_symbols  = None
+        self.target_plausibility = None
 
-		self.ngram               = None
-		self.M                   = None
+        self.ngram               = None
+        self.M                   = None
 
-		self.deciphered_text     = None
+        self.deciphered_text     = None
 
-	def set_target_plausibility(self, corpus, corpus_len, unique_symbols, \
-								ngram, M):
-		"""
-		parameters:
-			all are taken from the TextStats class
-		"""
+    def set_target_plausibility(self, corpus, corpus_len, unique_symbols, \
+                                ngram, M):
+        """
+        parameters:
+            all are taken from the TextStats class
+        """
 
-		self.unique_symbols     = unique_symbols
-		self.num_unique_symbols = len(self.unique_symbols)
-		self.ngram              = ngram
-		self.M                  = M
-		# set target plausibility
-		start = np.random.randint(corpus_len - self.ciphered_text_len)
-		target_text = corpus[start : start + self.ciphered_text_len]
-		target_numeric = text_to_numeric(self.unique_symbols, target_text)
-		self.target_plausibility = self.log_plausibility(target_numeric)
+        self.unique_symbols     = unique_symbols
+        self.num_unique_symbols = len(self.unique_symbols)
+        self.ngram              = ngram
+        self.M                  = M
+        # set target plausibility
+        start = np.random.randint(corpus_len - self.ciphered_text_len)
+        target_text = corpus[start : start + self.ciphered_text_len]
+        target_numeric = text_to_numeric(self.unique_symbols, target_text)
+        self.target_plausibility = self.log_plausibility(target_numeric)
 
-		return None
+        return None
 
-	def run(self):
-		self.ciphered_numeric = text_to_numeric(self.unique_symbols,\
-												self.ciphered_text)
-		# randomly choose initial states for each sample
-		deciphers = [np.random.permutation(self.num_unique_symbols) \
-					 for sample in range(self.num_samples)]
-		# calculate plausibility for initial states
-		current_plausibilities = [self.log_plausibility(apply_cipher(decipher,\
-								  self.ciphered_numeric)) \
-								  for decipher in deciphers]
-		plausibility_hist = np.zeros([self.num_steps, self.num_samples])
+    def run(self):
+        self.ciphered_numeric = text_to_numeric(self.unique_symbols,\
+                                                self.ciphered_text)
+        # randomly choose initial states for each sample
+        deciphers = [np.random.permutation(self.num_unique_symbols) \
+                     for sample in range(self.num_samples)]
+        # calculate plausibility for initial states
+        current_plausibilities = [self.log_plausibility(apply_cipher(decipher,\
+                                  self.ciphered_numeric)) \
+                                  for decipher in deciphers]
+        plausibility_hist = np.zeros([self.num_steps, self.num_samples])
 
-		for step in range(self.num_steps):
-			for sample in range(self.num_samples):
-				evolution_results             = self.evolve(deciphers[sample],\
-												current_plausibilities[sample])
-				deciphers[sample]               = evolution_results[0]
-				current_plausibilities[sample]  = evolution_results[1]
-				plausibility_hist[step, sample] = current_plausibilities[sample]
+        for step in range(self.num_steps):
+            for sample in range(self.num_samples):
+                evolution_results             = self.evolve(deciphers[sample],\
+                                                current_plausibilities[sample])
+                deciphers[sample]               = evolution_results[0]
+                current_plausibilities[sample]  = evolution_results[1]
+                plausibility_hist[step, sample] = current_plausibilities[sample]
 
-			# get greatest plausibility
-			index = np.argmax(current_plausibilities)
-			max_plausibility = current_plausibilities[index]
+            # get greatest plausibility
+            index = np.argmax(current_plausibilities)
+            max_plausibility = current_plausibilities[index]
 
-			if np.exp(max_plausibility - self.target_plausibility) > 0.999:
-				break
+            if np.exp(max_plausibility - self.target_plausibility) > 0.999:
+                break
 
-		print("Best sample is number {}.\n".format(index))
-		plausibility_hist = plausibility_hist[:step, :]
+        print("Best sample is number {}.\n".format(index))
+        plausibility_hist = plausibility_hist[:step, :]
 
-		self.decipher_text(deciphers[index], self.ciphered_numeric)
+        self.decipher_text(deciphers[index], self.ciphered_numeric)
 
-		return None
+        return None
 
-	def log_plausibility(self, message_numeric):
-		plausibility = 0
-		for i in range(self.ciphered_text_len - self.ngram + 1):
-			# sum because we took log of M to avoid roundoff errors
-			plausibility += self.M[message_numeric[i: i + self.ngram]]
-		return plausibility
+    def log_plausibility(self, message_numeric):
+        plausibility = 0
+        for i in range(self.ciphered_text_len - self.ngram + 1):
+            # sum because we took log of M to avoid roundoff errors
+            plausibility += self.M[message_numeric[i: i + self.ngram]]
+        return plausibility
 
-	def evolve(self, decipher, current_plausibility):
-		# randomly choose neighbor from (num_unique_symbols choose 2) neighbors
-		i, j = np.random.randint(self.num_unique_symbols, size = 2)
-		# make a transposition
-		decipher[i], decipher[j] = decipher[j], decipher[i]
+    def evolve(self, decipher, current_plausibility):
+        # randomly choose neighbor from (num_unique_symbols choose 2) neighbors
+        i, j = np.random.randint(self.num_unique_symbols, size = 2)
+        # make a transposition
+        decipher[i], decipher[j] = decipher[j], decipher[i]
 
-		proposed_deciphered_numeric = apply_cipher(decipher, \
-												   self.ciphered_numeric)
-		proposed_plaus = self.log_plausibility(proposed_deciphered_numeric)
-		plausibility_diff = proposed_plaus - current_plausibility
-		if plausibility_diff > 0:
-			# move to neighbor
-			current_plausibility = proposed_plaus
-		else:
-			acceptance_ratio = np.e**plausibility_diff
-			r = np.random.rand()
-			if r <= acceptance_ratio:
-				# move to neighbor
-				current_plausibility = proposed_plaus
-			else:
-				# stay; so we need to unflip transposition
-				decipher[i], decipher[j] = decipher[j], decipher[i]
+        proposed_deciphered_numeric = apply_cipher(decipher, \
+                                                   self.ciphered_numeric)
+        proposed_plaus = self.log_plausibility(proposed_deciphered_numeric)
+        plausibility_diff = proposed_plaus - current_plausibility
+        if plausibility_diff > 0:
+            # move to neighbor
+            current_plausibility = proposed_plaus
+        else:
+            acceptance_ratio = np.e**plausibility_diff
+            r = np.random.rand()
+            if r <= acceptance_ratio:
+                # move to neighbor
+                current_plausibility = proposed_plaus
+            else:
+                # stay; so we need to unflip transposition
+                decipher[i], decipher[j] = decipher[j], decipher[i]
 
-		return decipher, current_plausibility
+        return decipher, current_plausibility
 
-	def decipher_text(self, best_cipher, ciphered_numeric):
-		deciphered_numeric   = apply_cipher(best_cipher, ciphered_numeric)
-		deciphered_text      = numeric_to_text(self.unique_symbols, \
-											   deciphered_numeric)
-		self.deciphered_text = deciphered_text
+    def decipher_text(self, best_cipher, ciphered_numeric):
+        deciphered_numeric   = apply_cipher(best_cipher, ciphered_numeric)
+        deciphered_text      = numeric_to_text(self.unique_symbols, \
+                                               deciphered_numeric)
+        self.deciphered_text = deciphered_text
 
-		return None
+        return None
--- stokepy/text_stats.py
+++ stokepy/text_stats.py
@@ -1,92 +1,92 @@
 from .helpers import *
 
 class TextStats:
-	"""
-	Assorted statistical functions on a corpus
+    """
+    Assorted statistical functions on a corpus
 
-	parameters:
-		corpus (str) = a reference text
-	"""
+    parameters:
+        corpus (str) = a reference text
+    """
 
-	def __init__(self, corpus):
-		self.corpus             = corpus
-		self.unique_symbols     = None
-		self.unique_words       = None
-		self.ciphered_numeric   = None
-		self.num_unique_symbols = None
-		self.num_unique_words   = None
-		self.corpus_len         = None
-		self.ngram              = None
+    def __init__(self, corpus):
+        self.corpus             = corpus
+        self.unique_symbols     = None
+        self.unique_words       = None
+        self.ciphered_numeric   = None
+        self.num_unique_symbols = None
+        self.num_unique_words   = None
+        self.corpus_len         = None
+        self.ngram              = None
 
-	def clean_corpus(self, remove_chars):
-		"""
-		parameters:
-			remove_chars (array) = characters you want to be removed from corpus
-		"""
+    def clean_corpus(self, remove_chars):
+        """
+        parameters:
+            remove_chars (array) = characters you want to be removed from corpus
+        """
 
-		# clean corpus
-		self.corpus = self.corpus.lower()
-		self.corpus = ''.join(str(ch) if ch not in remove_chars else ' ' \
-							  for ch in self.corpus)
-		corpus_len  = len(self.corpus)
+        # clean corpus
+        self.corpus = self.corpus.lower()
+        self.corpus = ''.join(str(ch) if ch not in remove_chars else ' ' \
+                              for ch in self.corpus)
+        corpus_len  = len(self.corpus)
 
-		self.corpus_len         = corpus_len
-		# get unique symbols in corpus
-		unique_symbols          = sorted(list(set(self.corpus)))
-		unique_words            = sorted(list(set(self.corpus.split())))
-		self.unique_words       = unique_words
-		self.num_unique_words   = len(unique_words)
-		self.unique_symbols     = unique_symbols
-		num_unique_symbols      = len(unique_symbols)
-		self.num_unique_symbols = num_unique_symbols
+        self.corpus_len         = corpus_len
+        # get unique symbols in corpus
+        unique_symbols          = sorted(list(set(self.corpus)))
+        unique_words            = sorted(list(set(self.corpus.split())))
+        self.unique_words       = unique_words
+        self.num_unique_words   = len(unique_words)
+        self.unique_symbols     = unique_symbols
+        num_unique_symbols      = len(unique_symbols)
+        self.num_unique_symbols = num_unique_symbols
 
 
-		self.corpus_numeric = None
-		self.ngram = None
+        self.corpus_numeric = None
+        self.ngram = None
 
-		return None
+        return None
 
-	def set_corpus_frequencies(self, ngram, groupby = 'words', log = False):
-		"""
-		parameters:
-			ngram (int)
-			groupby (str) = options: 'chars', 'words'; default is 'words'
-			log (bool) = values of frequency 'matrix'
-		"""
+    def set_corpus_frequencies(self, ngram, groupby = 'words', log = False):
+        """
+        parameters:
+            ngram (int)
+            groupby (str) = options: 'chars', 'words'; default is 'words'
+            log (bool) = values of frequency 'matrix'
+        """
 
-		if groupby == 'words':
-			self.corpus = self.corpus.split(' ')
-			# remove all ''
-			indexes = [i for i, word in enumerate(self.corpus) if word == '']
-			offset = 0
-			for index in indexes:
-				del self.corpus[index-offset]
-				offset+=1
-		elif groupby == 'chars':
-			self.corpus = list(self.corpus)
-		else:
-			raise ValueError('{} is not supported'.format(groupby))
+        if groupby == 'words':
+            self.corpus = self.corpus.split(' ')
+            # remove all ''
+            indexes = [i for i, word in enumerate(self.corpus) if word == '']
+            offset = 0
+            for index in indexes:
+                del self.corpus[index-offset]
+                offset+=1
+        elif groupby == 'chars':
+            self.corpus = list(self.corpus)
+        else:
+            raise ValueError('{} is not supported'.format(groupby))
 
-		if groupby == 'chars':
-			corpus_numeric = text_to_numeric(self.unique_symbols, self.corpus)
-			dimensions = np.repeat(self.num_unique_symbols, ngram)
-		else:
-			corpus_numeric = text_to_numeric(self.unique_words, self.corpus)
-			dimensions = np.repeat(self.num_unique_words, ngram)
+        if groupby == 'chars':
+            corpus_numeric = text_to_numeric(self.unique_symbols, self.corpus)
+            dimensions = np.repeat(self.num_unique_symbols, ngram)
+        else:
+            corpus_numeric = text_to_numeric(self.unique_words, self.corpus)
+            dimensions = np.repeat(self.num_unique_words, ngram)
 
-		if log:
-			# initialize M with 1s because log function doesn't like 0s
-			M = np.ones(dimensions, int)
-		else:
-			M = np.zeros(dimensions, int)
-		# count frequencies
-		for i in range(self.corpus_len - ngram + 1):
-			M[corpus_numeric[i:i + ngram]] += 1
-		# normalize last dimension
-		M = np.apply_along_axis(normalize_vector, -1, M)
-		if log:
-			# log all frequencies to avoid floating point error
-			M = np.log(M)
-		self.M = M
+        if log:
+            # initialize M with 1s because log function doesn't like 0s
+            M = np.ones(dimensions, int)
+        else:
+            M = np.zeros(dimensions, int)
+        # count frequencies
+        for i in range(self.corpus_len - ngram + 1):
+            M[corpus_numeric[i:i + ngram]] += 1
+        # normalize last dimension
+        M = np.apply_along_axis(normalize_vector, -1, M)
+        if log:
+            # log all frequencies to avoid floating point error
+            M = np.log(M)
+        self.M = M
 
-		return None
+        return None
